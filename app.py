6a63578e2db5d8390fee613a11f79653fadfd853
YuRuiii <1754376900@qq.com>
2023-08-21 01:46:15 +0800

    add app

--- app.py
+++ app.py
@@ -1,25 +1,47 @@
+from stable_baselines3 import A2C
+from stable_baselines3 import DDPG
+from stable_baselines3 import PPO
+from stable_baselines3 import SAC
+from stable_baselines3 import TD3                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
 from flask import Flask, request, jsonify
+from env import TradingEnv
+
+MODELS = {
+    "A2C": A2C,
+    "DDPG": DDPG,
+    "PPO": PPO,
+    "SAC": SAC,
+    "TD3": TD3,
+}
 
 app = Flask(__name__)
-
-
-@app.route('/learn')
-def learn():
-    data = request.get_json()
-    agent = data['algorithm']
-    if agent.lower() == 'ppo':
+env = TradingEnv()
+class Agent:
+    def __init__(self, env):
+        self.env = env
+        self.model = self.create_model()
         
-    else:
-        assert 0
+    @app.route('/create-model', methods=['POST'])
+    def create_model(self):
+        data = request.get_json()
+        agent = data['algorithm']
+        if agent.upper() == 'PPO':
+            model = PPO("MlpPolicy", env, verbose=1)
+        else:
+            assert 0
+        return model
+
+    @app.route('/learn')
+    def learn(self):
+        self.model.learn(total_timesteps=10000)
+        return self.model
+    
+    @app.route('/predict', methods=['POST'])
+    def predict(self):
+        data = request.get_json()
         
-    
-    
-@app.route()
-    
-@app.route('/predict', methods=['POST'])
-def predict():
-    data = request.get_json()
-    
-    model_path = data['model_path']
-    
+        action, _ = self.model.predict(data)
+        return action
+
+app.run(port=8080)
     
